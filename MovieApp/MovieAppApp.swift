b499cac1a25ad8114449d90182acf5734bb67fa7
jose pablo sanchez <josepsan2424@gmail.com>
2026-02-22 11:27:28 -0600

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by jose pablo sanchez guillen on 22/02/26.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
